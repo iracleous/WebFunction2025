5a44d0d68220a47737efa083f4b592c54ec96fd9
Dimitris Iracleous <diracleous@athtech.gr>
2025-06-04 20:09:11 +0300

    a1

--- WebFunction2025/Function1.cs
+++ WebFunction2025/Function1.cs
@@ -1,14 +1,7 @@
-using Azure;
-using Azure.Data.Tables;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
-using Microsoft.Extensions.Diagnostics.HealthChecks;
-using Microsoft.Extensions.Logging;
- 
-using Newtonsoft.Json;
-using System.Net;
-using WebFunction2025.Models;
+
 
 namespace WebFunction2025;
 
